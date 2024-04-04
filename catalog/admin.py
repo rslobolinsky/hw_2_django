7ae5a0f9df171da58646233aef179eda58d95a98
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-04 14:30:14 +0400

    create admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from catalog.models import Product
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ()
+    list_filter = ()
+    search_fields = ()
