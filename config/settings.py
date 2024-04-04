47ceb6ecbbbdd3a0ee908b7fbb602a2014cfacff
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-04 11:48:59 +0400

    Create database, add changes in settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'Database_hw_3_Django',
+        'USER': 'postgres',
+        'PASSWORD': '1234'
     }
 }
 
