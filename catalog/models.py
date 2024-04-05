bc483078b037e7e9eebd4b4f6b631e470670ddb9
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-05 09:47:23 +0400

    fix code

--- catalog/models.py
+++ catalog/models.py
@@ -24,7 +24,8 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name='Цена за покупку')
     created_at = models.DateTimeField(**NULLABLE, verbose_name='Дата создания (записи в БД)')
     updated_at = models.DateTimeField(**NULLABLE, verbose_name='Дата последнего изменения (записи в БД)')
-    manufactured_at = models.DateTimeField(**NULLABLE, verbose_name='Дата производства продукта')
+
+    # manufactured_at = models.DateTimeField(**NULLABLE, verbose_name='Дата производства продукта')
 
     def __str__(self):
         return f'{self.name} ({self.description}) ({self.category}) ({self.price})'
