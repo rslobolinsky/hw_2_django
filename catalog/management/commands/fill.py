e6aff046984cbff7fdcccad680c47ec63467d9a1
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-05 09:47:07 +0400

    add custom command which fill poructs and categories

---
+++ catalog/management/commands/fill.py
@@ -0,0 +1,51 @@
+import json
+
+from django.core.management import BaseCommand
+
+from catalog.models import Product, Category
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories():
+        with open('data.json', encoding='utf-8') as file:
+            data = json.load(file)
+        return [item for item in data if item('model') == 'catalog.category']
+
+    @staticmethod
+    def json_read_products():
+        with open('data.json', encoding='utf-8') as file:
+            data = json.load(file)
+        return [item for item in data if item('model') == 'catalog.product']
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        product_for_create = []
+        category_for_create = []
+
+        # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
+        for category in Command.json_read_categories():
+            category_for_create.append(
+                Category(id=category['pk'],
+                         name=category['fields']['name'],
+                         description=category['fields']['description'])
+            )
+
+        # Создаем объекты в базе с помощью метода bulk_create()
+        Category.objects.bulk_create(category_for_create)
+
+        # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
+        for product in Command.json_read_products():
+            product_for_create.append(
+                Product(id=product["pk"],
+                        name=product["fields"]["name"],
+                        description=product["fields"]["description"],
+                        category=Category.objects.get(pk=product["fields"]["category"]),
+                        price=product["fields"]["price"])
+            )
+
+        # Создаем объекты в базе с помощью метода bulk_create()
+        Product.objects.bulk_create(product_for_create)
