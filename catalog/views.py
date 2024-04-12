c0dab6e61bd21da75c3eec11f57576afabfb3116
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-12 15:08:43 +0400

    add controllers and samples

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
+from catalog.models import Category
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    category_list = Category.objects.all()
+    context = {
+        'object_list': category_list,
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
