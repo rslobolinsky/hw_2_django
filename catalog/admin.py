2e2c8dc5e040bf5cada9064e7d159af07ca11da5
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-12 17:15:52 +0400

    fix code

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,11 +7,11 @@ from catalog.models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('pk', 'name')
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('pk', 'name', 'price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description',)
