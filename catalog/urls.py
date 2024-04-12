a55bc6e4209db6ed01239bd2a5d4ef606c3ffd47
Roman Slobolinsky <rslobolinsky13@gmail.com>
2024-04-12 17:27:23 +0400

    fix code

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from catalog.views import home, contacts, category_list
+from catalog.views import home, contacts, category_list, category_product, product
 
 urlpatterns = [
     path('', home, name='home'),
-    path('category/', category_list, name='categories'),
+    path('category/', category_list, name='category'),
+    path('category/<int:pk>_category/', category_product, name='category_product'),
+    path('products/<int:pk>_product/', product, name='product'),
     path('contacts/', contacts, name='contacts')
 ]
